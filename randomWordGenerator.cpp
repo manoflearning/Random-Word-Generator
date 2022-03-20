a9e9ac299efa8ce166fad47f8e239df318db8a17
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-03-20 09:28:29 +0900

    Add files via upload

---
+++ randomWordGenerator.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int cntWords = 5886;
+const int len = 54;
+
+vector<string> words;
+
+void input() {
+    for (int i = 0; i < cntWords; i++) {
+        string word; cin >> word;
+        words.push_back(word);
+    }
+}
+
+void output() {
+    srand(time(NULL));
+
+    vector<int> visited(cntWords);
+
+    for (int i = 0; i < len; i++) {
+        int x = rand() % cntWords;
+        while (visited[x]) {
+            x = rand() % cntWords;
+        }
+        visited[x] = true;
+    
+        cout << i + 1 << "번째 단어: " << words[rand() % cntWords];
+        cout << '\n';
+    }
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    
+    input();
+
+    output();
+
+    return 0;
+}
